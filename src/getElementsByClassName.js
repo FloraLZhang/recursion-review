6d6e2ee0da6bc7f73a958bc9df9485f6f37a4e6b
Flora Zhang <Azurstyle@outlook.com>
2024-01-22 20:09:12 -0500

    Pass Tests for stringifyJSON and getElementsbyClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className,node) {
+  var result = [];
+  // check if node exists, if not asign it to document.body
+  if (node === undefined) {
+    node = document.body;
+  }
+  // check node type, if its 1, cheeck for class, push if yes
+  if (node.nodeType === 1 && node.classList && node.classList.contains(className)) {
+      result.push(node);
+    }
+
+  // check for childnodes, iterate through childnodes
+  if (node.hasChildNodes) {
+    for (var i = 0; i < node.childNodes.length; i ++) {
+      var childResults = getElementsByClassName(className, node.childNodes[i]);
+      result = result.concat(childResults);
+    }
+  }
+  return result;
 };
+//   var result = [];
+
+//   var searchNode = function(node) {
+//     // Check if the current node has a classList and if that classList contains the className
+//     if (node.classList && node.classList.contains(className)) {
+//       result.push(node);
+//     }
+
+//     // Recursively search through child nodes
+//     node.childNodes.forEach(function(child) {
+//       searchNode(child);
+//     });
+//   };
+
+//   // Start the search with document.body as the root node
+//   searchNode(document.body);
+//   return result;
+// }
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // console.log("stringifyJSON function is called");
+
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean'){
+    return obj.toString();
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+        result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+}
+
+
+  if (typeof obj === 'object') {
+    var props = [];
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+          props.push('"' + key + '":' + stringifyJSON(obj[key]));
+        }
+      }
+    }
+    return '{' + props.join(',') + '}';
+  }
+  return undefined;
 };
